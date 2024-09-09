6a75549395b2ff01c34f9b984cdc7fcead2b9fef
Harold GR <harold@girres.com.co>
2024-09-09 23:13:54 +0200

    footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,18 +6,18 @@ const Comp = ({ dict = {} }) => {
   return (
     <footer className='bg-mm-black text-mm-beige'>
       <div className='mm-container lg:flex items-start justify-between p-4'>
-        <div className='w-full lg:w-[50%] grid grid-cols-3 gap-5 items-start justify-between'>
-          <div className='text-left text-sm'>
+        <div className='w-full lg:w-[50%] grid grid-cols-2 lg:grid-cols-3 gap-5 items-start justify-between'>
+          <div className='text-center lg:text-left text-sm'>
             <h2 className='text-mm-orange font-bold text-sm lg:text-lg mb-4'>
               {data?.title1 ?? 'Ubicaciones'}
             </h2>
-            <ul className='text-left'>
+            <ul>
               <li>Madrid</li>
               <li>Miami</li>
               <li>Bogotá</li>
             </ul>
           </div>
-          <div className='text-left text-sm'>
+          <div className='text-center lg:text-left text-sm'>
             <h2 className='text-mm-orange font-bold text-sm lg:text-lg mb-4'>
               {data?.title2 ?? 'Síguenos'}
             </h2>
@@ -34,25 +34,8 @@ const Comp = ({ dict = {} }) => {
               </li>
             </ul>
           </div>
-          <div className='text-left text-sm hidden'>
-            <h2 className='text-mm-orange font-bold text-sm lg:text-lg mb-4'>
-              {data?.title3 ?? 'Legal'}
-            </h2>
-            <ul>
-              <li>
-                <Link href='/terms' target='_blank'>
-                  Términos y condiciones
-                </Link>
-              </li>
-              <li>
-                <Link href='/privacy' target='_blank'>
-                  Privacidad
-                </Link>
-              </li>
-            </ul>
-          </div>
         </div>
-        <div className='w-full lg:w-[50%] text-right text-sm'>
+        <div className='w-full lg:w-[50%] text-center lg:text-right text-sm'>
           <div className='divider block lg:hidden' />
           <ul>
             <li>
